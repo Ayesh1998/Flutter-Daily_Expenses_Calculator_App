47ced9797cb617d99d690237177c1d596ac976de
Ayesh1998 <ayeshlak1998@gmail.com>
2020-04-27 15:57:55 +0530

    Created the app using flutter and removed initial code and added a appBar

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dialy Expenses',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daily Expenses'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
