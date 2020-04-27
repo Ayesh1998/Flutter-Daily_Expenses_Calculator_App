604705fddd502b5b7060a560e9a1364a8d831ba1
Ayesh1998 <ayeshlak1998@gmail.com>
2020-04-27 18:20:13 +0530

    Implemented homepage and created two sections

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:daily_expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -15,14 +16,52 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
-
+  final List<Transaction> transactions = [
+    Transaction(
+      amount: 200.00,
+      date: DateTime.now(),
+      id: 't1',
+      title: 'Buy a car',
+    ),
+    Transaction(
+      amount: 100.00,
+      date: DateTime.now(),
+      id: 't2',
+      title: 'Buy a laptop',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Daily Expenses'),
         centerTitle: true,
+        backgroundColor: Colors.teal[700],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.teal[300],
+                child: Text(
+                  'Chart',
+                  style: TextStyle(),
+                ),
+                elevation: 5,
+              ),
+            ),
+            Column(
+                children: transactions.map((transaction) {
+              return Card(
+                child: Text(transaction.title),
+              );
+            }).toList()),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/models/transaction.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    @required this.amount,
+    @required this.date,
+    @required this.id,
+    @required this.title,
+  });
+}
